ee88ecf7de7cc5e48a721f5d52e6f17200f0b164
Patrik Papso <bluexik@gmx.com>
2018-07-05 14:26:11 +0200

    Added simple player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+
+    [SerializeField] private float jumpTakeOffSpeed = 7f;
+    [SerializeField] private float maxSpeed = 7f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && _grounded)
+        {
+            _velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (_velocity.y > 0) _velocity.y = _velocity.y * .5f;
+        }
+        _targetVelocity = move * maxSpeed;
+    }
+}
