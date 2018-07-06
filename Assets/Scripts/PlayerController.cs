0ff25e1760b07c2bec4d1787d7080d9350ea4146
Patrik Papso <papso14@student.fiit.stuba.sk>
2018-07-06 21:34:54 +0200

    Added checkbox to allow player to fly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : PhysicsObject
 
     [SerializeField] private float jumpTakeOffSpeed = 7f;
     [SerializeField] private float maxSpeed = 7f;
-
+    [SerializeField] private bool canFly = false;
     // Use this for initialization
     void Start()
     {
@@ -19,7 +19,7 @@ public class PlayerController : PhysicsObject
         Vector2 move = Vector2.zero;
         move.x = Input.GetAxis("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && (_grounded || canFly))
         {
             _velocity.y = jumpTakeOffSpeed;
         }
