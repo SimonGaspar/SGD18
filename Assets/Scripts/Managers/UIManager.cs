59e875a6459903c8f1250d78f27a8b0f76a27787
Patrik Papso <bluexik@gmx.com>
2018-07-17 14:19:44 +0200

    Added UI Panels + Modal window

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -10,8 +10,10 @@ public class UIManager : Singleton<UIManager>
     [Space]
     [Header("Top left UI")]
     [SerializeField] private GameObject _buttonPrefab;
-    [SerializeField] private Vector2 _offset = new Vector2(39f, -79f);
+    [SerializeField] private Vector2 _startPosition = new Vector2(39f, -105f);
+    [SerializeField] private Vector2 _offsetEachButton = new Vector2(0f, 205);
     [SerializeField] private Animal[] _animals;
+    [SerializeField] private ModalWindowScript _modalScript;
 
     [Header("Panel")]
     [SerializeField] private GameObject _panelPrefab;
@@ -23,34 +25,44 @@ public class UIManager : Singleton<UIManager>
         Assert.IsNotNull(_buttonPrefab);
         Assert.IsNotNull(_panelPrefab);
 
-        SpawnButtons();
+        SpawnMainMenu();
+        SpawnSideButtons();
+    }
+    private void SpawnMainMenu()
+    {
+
     }
 
-    private void SpawnButtons()
+    private void SpawnSideButtons()
     {
         Vector3 buttonPosition = Vector3.zero;
-        buttonPosition.x = _offset.x;
-        buttonPosition.y = -38;
+        buttonPosition = new Vector3(_startPosition.x, _startPosition.y, 0f);
+
         foreach (Animal a in _animals)
         {
             GameObject b = Instantiate(_buttonPrefab);
 
             b.GetComponent<Image>().sprite = a.AnimalSprite;
-            b.GetComponent<Button>().onClick.AddListener(delegate { HandleClick(a); });
+            b.GetComponent<Button>().onClick.AddListener(delegate { HandleButtonClick(a); });
             b.transform.position = buttonPosition;
             b.transform.SetParent(_canvas.transform, false);
 
-            buttonPosition.y += _offset.y;
+            buttonPosition += new Vector3(_offsetEachButton.x, _offsetEachButton.y, 0f);
         }
     }
 
-    private void HandleClick(Animal a)
+    private void HandleButtonClick(Animal animal)
     {
         if (_currentlyOpenedPanel != null) Destroy(_currentlyOpenedPanel);
 
         GameObject panel = Instantiate(_panelPrefab);
         panel.transform.SetParent(_canvas.transform, false);
-        panel.GetComponent<UIPanelScript>().InitializePanel(a);
+        panel.GetComponent<UIPanelScript>().InitializePanel(_animals, animal);
         _currentlyOpenedPanel = panel;
+    }
+
+    public void ShowModal()
+    {
+        _modalScript.SetUpModal("Hello there", "YES", delegate { _modalScript.HideModal(); }, "NO", delegate { _modalScript.HideModal(); });
     }
 }
