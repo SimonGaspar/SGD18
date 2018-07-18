f31b9727c6257aa7efa4a1cd4de35d30acb26c8a
Patrik Papso <bluexik@gmx.com>
2018-07-18 14:27:10 +0200

    Added simple pick-up animation (change tho)

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -6,17 +6,26 @@ public enum CollectibleType { Feather, Arm };
 public class Collectible : MonoBehaviour
 {
     [SerializeField] private CollectibleType _type;
+    private Animator _animator;
 
     private bool _picked = false;
 
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player" && !_picked)
         {
             GameManager.Instance.CollectiblePickup(_type);
             _picked = true;
-            // play animation
-            Destroy(gameObject);
+            _animator.SetTrigger("isPicked");
         }
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
